c2989d56a389459b08f80db7d6efad4c20ea40b6
Oscar Andersson <oscar.n.andersson135ggmail.com>
2022-04-20 20:30:05 +0200

    glömde pusha

--- anagrams.java
+++ anagrams.java
@@ -1,9 +1,24 @@
 import java.util.Scanner;
+// import java.io.*;
+// import java.util.Arrays;
 
 public class anagrams {
 
     static boolean isAnagram(String a, String b) {
         // Complete the function
+        char[] chArrayA = a.toLowerCase().toCharArray();
+        char[] chArrayB = b.toLowerCase().toCharArray();
+        java.util.Arrays.sort(chArrayA);
+        java.util.Arrays.sort(chArrayB);
+        if (chArrayA.length != chArrayB.length) {
+            return false;
+        }
+        for (int i = 0; i < chArrayB.length; i++) {
+            if (chArrayA[i] != chArrayB[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
---
+++ string_tokens.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class string_tokens {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        
+        scan.close();
+    }
+}
+
